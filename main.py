f93ede482902086564055fe67bac57a171ec2211
Navid <navidjalili3@gmail.com>
2024-02-04 19:11:23 +0330

    Add Transposition cipher
    All the credits to Al Sweigart

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from caesar_cipher.caesar_code_breaker import decipher
+from transposition_cipher.transposition import TranspositionCipher
+
+
+def main() -> None:
+    print(TranspositionCipher.encrypt("Common sense is not so common.", 8))
+    print(TranspositionCipher.decrypt(
+        "Cenoonommstmme oo snnio. s s c", 8))
+
 
 if __name__ == "__main__":
-    print(decipher("FDW"))
+    main()
