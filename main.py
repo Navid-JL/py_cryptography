f680b35b49678c39840ce0fe4a47ccb0cd65aef4
Navid <navidjalili3@gmail.com>
2024-02-17 01:03:22 +0330

    Use a dictionary attack to brute-force the Vigenere cipher
    Move 'dictionary.txt' file to the main path (/)

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import time
+# mypy: disable-error-code="import-untyped"
+import pyperclip
+
+from vigenere_cipher.vinegere_dict_breaker import vigenere_dict_break
 
 
 def main() -> None:
+    ciphertext = """Tzx isnz eccjxkg nfq lol mys bbqq I lxcz."""
+    hackedMessage = vigenere_dict_break(ciphertext)
 
-    startTime = time.time()
-    for trial in range(10_000):
-        building_str = ''
-        for i in range(10_000):
-            building_str += 'x'
-    print('String concatenation: ', (time.time() - startTime))
-
-    startTime = time.time()
-    for trial in range(10_000):
-        building_list = []
-        for i in range(10_000):
-            building_list.append('x')
-        building_str = ''.join(building_list)
-    print('List appending: ', (time.time() - startTime))
+    if hackedMessage != None:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print('Failed to hack encryption.')
 
 
 if __name__ == '__main__':
